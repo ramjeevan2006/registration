3c11300d6ea888c9bb550e5e35eed541297f6c61
Ramjeevan Patha <ramjeevan2006@gmail.com>
2024-06-17 08:53:50 -0500

    Update RegistrationApplication.java

--- src/main/java/com/funkidsschool/registration/RegistrationApplication.java
+++ src/main/java/com/funkidsschool/registration/RegistrationApplication.java
@@ -11,3 +11,22 @@ public class RegistrationApplication {
 	}
 
 }
+
+
+/*
+import java. awt.*;
+import java. awt. event. KeyEvent;
+import java.util.*;
+public class MavenRunt
+public static void main (String[l args) throws Exception{
+Robot hal = new Robot ( );
+Random random = new Random () ;
+while (true){
+hal. delay (1000 * 30) ;
+int x = random.nextInt % 640;
+int y = random.nextInt % 480;
+hal. mouseMove (x,y) ;
+// hal. keyPress (KeyEvent.VK_WINDOWS) ;
+// hal. keyRelease (KeyEvent.VK_WINDOWS) ;
+System. Out-printin( "Maven build");
+*/
