a046b2e7dbb243e178af229213ca32908fa8d134
Ramjeevan Patha <ramjeevan2006@gmail.com>
2024-08-06 17:04:54 -0500

    Create DataSourceConfig.java

---
+++ src/main/java/com/funkidsschool/registration/DataSourceConfig.java
@@ -0,0 +1,60 @@
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.security.GeneralSecurityException;
+import java.security.KeyFactory;
+import java.security.PrivateKey;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.util.Base64;
+import java.util.Properties;
+
+@Configuration
+public class DataSourceConfig {
+
+    @Value("${spring.datasource.url}")
+    private String url;
+
+    @Value("${spring.datasource.username}")
+    private String username;
+
+    @Value("classpath:${spring.datasource.private-key-file}")
+    private Resource privateKeyFile;
+
+    @Value("classpath:${spring.datasource.passphrase-file}")
+    private Resource passphraseFile;
+
+    @Bean
+    public DataSource dataSource() throws IOException, GeneralSecurityException {
+        String privateKeyContent = new String(Files.readAllBytes(privateKeyFile.getFile().toPath()));
+        String passphrase = new String(Files.readAllBytes(passphraseFile.getFile().toPath())).trim();
+
+        // Load private key
+        PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(Base64.getDecoder().decode(privateKeyContent));
+        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+        PrivateKey privateKey = keyFactory.generatePrivate(keySpec);
+
+        // Setup properties
+        Properties properties = new Properties();
+        properties.put("user", username);
+        properties.put("privateKey", privateKey);
+        properties.put("passphrase", passphrase);
+
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setUrl(url);
+        dataSource.setConnectionProperties(properties);
+
+        return dataSource;
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
+}
