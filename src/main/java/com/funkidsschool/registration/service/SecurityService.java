508e048d319121fc8d8864f6dc38e824f4bfb24d
Ramjeevan Patha <ramjeevan2006@gmail.com>
2024-09-09 14:30:52 -0500

    Update SecurityService.java

--- src/main/java/com/funkidsschool/registration/service/SecurityService.java
+++ src/main/java/com/funkidsschool/registration/service/SecurityService.java
@@ -1,14 +1,21 @@
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import java.util.List;
 
 @Service
 public class SecurityService {
 
     @Autowired
-    private SecurityRepository securityRepository;
+    private SessionFactory sessionFactory;
 
     public List<Security> getAllSecurities() {
-        return securityRepository.findAll();
+        try (Session session = sessionFactory.openSession()) {
+            return session.createQuery("from Security", Security.class).list();
+        }
     }
+
+    // Other service methods can go here
 }
